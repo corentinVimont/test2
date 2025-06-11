44a1a8b73d6c6392b4f88a2fb35e64e3fe078022
VIMONT Corentin <cVimont@cisa-informatique.local>
2025-06-11 09:59:07 +0200

    test

---
+++ animal.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testapp
+{
+    internal class animal
+    {
+        public string espece { get; set; }
+    }
+}
